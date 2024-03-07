09d5cfda5c90b87b5d4f60dba97e564245db83ef
greg <gregory.silvestri45@gmail.com>
2024-03-07 12:04:01 +0100

    Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+//import pour changer la local de mon app
+import * as fr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,15 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    //providers pour passer mes valeurs en local fr
+    { provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    //use registerLocaleData pour envoyer 'information à mon app
+    registerLocaleData(fr.default);
+  }
+}
